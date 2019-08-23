e29566d3a4eeda52d0b88df88e877897bd453048
Alex <105612551@qq.com>
2019-08-23 10:54:36 +0800

    test for checkout

---
+++ Singleton/TestForCheckout.h
@@ -0,0 +1,17 @@
+//
+//  TestForCheckout.h
+//  Singleton
+//
+//  Created by Alex on 2019/8/23.
+//  Copyright © 2019 xiaojia.xiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestForCheckout : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Singleton/TestForCheckout.m
@@ -0,0 +1,13 @@
+//
+//  TestForCheckout.m
+//  Singleton
+//
+//  Created by Alex on 2019/8/23.
+//  Copyright © 2019 xiaojia.xiao. All rights reserved.
+//
+
+#import "TestForCheckout.h"
+
+@implementation TestForCheckout
+
+@end
