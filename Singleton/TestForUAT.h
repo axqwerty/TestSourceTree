64b09cba2ed2f7be869f73df1b6fc07b4bcef3ef
Alex <105612551@qq.com>
2019-08-23 11:08:21 +0800

    test for uat

---
+++ Singleton/TestForUAT.h
@@ -0,0 +1,17 @@
+//
+//  TestForUAT.h
+//  Singleton
+//
+//  Created by Alex on 2019/8/23.
+//  Copyright © 2019 xiaojia.xiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestForUAT : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Singleton/TestForUAT.m
@@ -0,0 +1,13 @@
+//
+//  TestForUAT.m
+//  Singleton
+//
+//  Created by Alex on 2019/8/23.
+//  Copyright © 2019 xiaojia.xiao. All rights reserved.
+//
+
+#import "TestForUAT.h"
+
+@implementation TestForUAT
+
+@end
